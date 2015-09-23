87ee2cfaf5662ab5c462408751009b7d24e75d12
junannko <sabishinnbou.ookami@gmail.com>
2015-09-23 07:49:15 +0000

    sign up okay!

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to @user #ここを修正
+      flash[:success]="Welcome to the sample App !"
+      redirect_to @user
     else
       render 'new'
     end
